fa1e486b727085a973f289da4851e0ef9ddadace
Yevhen Shymka <schimka3@gmail.com>
2025-03-21 21:43:33 +0100

    3

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,13 @@
 function filterArray(numbers, value) {
-  const filterNumber = [];
+  const filteredNumbers = [];
 
   for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] > value) {
-      filterNumber.push([i]);
+      filteredNumbers.push(numbers[i]);
     }
   }
-  return filterNumber;
+
+  return filteredNumbers;
 }
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
 console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
